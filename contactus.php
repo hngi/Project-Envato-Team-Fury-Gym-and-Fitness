659cc533607d9eaf4090001452f988f4b0d4b0b3
Ninachars <66235310+Ninachars@users.noreply.github.com>
2020-06-22 07:02:52 +0100

    Create contactus.php

---
+++ contactus.php
@@ -0,0 +1,34 @@
+<?php
+
+if(isset($_POST['Send'])) {
+  $name = $_POST['Name'];
+  $mailFrom = $_POST['Email'];
+  $message = $_POST['Enter your message'];
+
+  if ($name === ''){
+    echo "Name cannot be empty."; die();
+  }
+
+  if ($email === ''){
+    echo "Email cannot be empty."; die();
+  }
+
+  else {
+    if (!filter_var($email, FILTER_VALIDATE_EMAIL)){
+      echo "Email format invalid."; die();
+    }
+  }
+
+  if ($message === ''){
+    echo "Message cannot be empty."; die();
+  }
+
+  $recipient = "okerekenina@gmail.com";
+  $mailheader = "From: ".$mailFrom;
+  $content = "You have received an email from ".$name.".\n\n".$message;
+
+  mail($recipient, $subject, $content, $mailheader) or die("Error!");
+
+  echo "Email sent!";
+  
+?>
